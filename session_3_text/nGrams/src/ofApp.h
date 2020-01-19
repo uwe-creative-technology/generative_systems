a2969091a6dc564da92d006dede1c7dcbf97c0d6
danb <dan@buzzo.com>
2020-01-20 00:23:51 +0100

    updated source text

--- session_3_text/nGrams/src/ofApp.h
+++ session_3_text/nGrams/src/ofApp.h
@@ -29,7 +29,7 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
-    string sourceText;
+    string sourceText, markovText;
     vector<string> allGrams;
     vector<vector<string>> uniqueGrams;
     int gramLength;
