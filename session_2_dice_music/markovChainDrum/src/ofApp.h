eed28cd24eb5a7121d4d47f5099f044a6adf3e31
danb <dan@buzzo.com>
2020-02-06 18:53:04 +0100

    initial commit

---
+++ session_2_dice_music/markovChainDrum/src/ofApp.h
@@ -0,0 +1,34 @@
+/*
+ Project Title: markovChainDrum
+ Description: markov chain drum machine
+ ©Daniel Buzzo 2020
+ dan@buzzo.com
+ http://buzzo.com
+ https://github.com/danbz
+ */
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxMarkovChain.h"
+
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+	
+    vector<ofSoundPlayer> samples;
+    ofxMC::MarkovChain markov;
+    int currentSound;
+    vector<int> playedList;
+};
